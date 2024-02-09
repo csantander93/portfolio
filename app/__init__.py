d29391e4cf3facdf6412c10ec7e78e2f24c585f5
Cristian Santander <casdesarrollo.2022@gmail.com>
2024-02-09 15:16:44 -0300

    app.config variables necesarias para el despliegue

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,14 @@ def create_app():
     app = Flask(__name__)
 
     app.config.from_mapping(
-        SENDGRID_KEY=os.environ.get('SENDGRID_KEY'),
-        DATABASE_PORT=os.environ.get('FLASK_DATABASE_PORT')
+        FROM_EMAIL=os.environ.get('FROM_EMAIL'),
+        SENDGRID_KEY=os.environ.get('SENDGRID_KEY'), 
+        SECRET_KEY=os.environ.get('SECRET_KEY'),
+        DATABASE_HOST=os.environ.get('FLASK_DATABASE_HOST'),
+        DATABASE_PASSWORD=os.environ.get('FLASK_DATABASE_PASSWORD'),
+        DATABASE_USER=os.environ.get('FLASK_DATABASE_USER'),
+        DATABASE=os.environ.get('FLASK_DATABASE'),
+        DATABASE_PORT=os.environ.get('FLASK_DATABASE_PORT'),
     )
 
     from . import portfolio
